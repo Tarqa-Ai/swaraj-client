e8854bc2c555a1cc75f43005e525f746e79ef30d
Sudhanshu Tiwari <sudhanshutiwari264@gmail.com>
2026-06-09 14:26:43 +0530

    fix: update backend

--- apps/backend/docker-entrypoint.sh
+++ apps/backend/docker-entrypoint.sh
@@ -4,4 +4,4 @@ set -e
 # Run database migrations using the local Prisma CLI binary
 ./apps/backend/node_modules/.bin/prisma migrate deploy --schema=apps/backend/prisma/schema.prisma
 
-exec node apps/backend/dist/main.js
+exec node apps/backend/dist/src/main.js
